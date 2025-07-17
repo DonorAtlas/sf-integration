c4fa6fa572503e21f1256fab13372da2525f9d1a
Aaron Berger <aaronzberger@gmail.com>
2025-07-17 10:44:00 -0400

    Before big change

--- donoratlas-testing-1/force-app/main/default/lwc/enhanceContact/enhanceContact.js
+++ donoratlas-testing-1/force-app/main/default/lwc/enhanceContact/enhanceContact.js
@@ -1,28 +1,34 @@
 import { LightningElement, api } from 'lwc';
 import startSingle from '@salesforce/apex/DonorAtlasService.startSingle';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { CloseActionScreenEvent } from 'lightning/actions';
 
 export default class EnhanceContact extends LightningElement {
     @api recordId;
 
-    handleClick() {
+    /** Called automatically for headless quick-actions */
+    @api invoke() {
         startSingle({ contactId: this.recordId })
             .then(() => {
                 this.dispatchEvent(
                     new ShowToastEvent({
                         title: 'DonorAtlas',
-                        message: 'Enrichment started',
+                        message: 'Enrichment job queued',
                         variant: 'success'
                     })
                 );
+                this.dispatchEvent(new CloseActionScreenEvent());
             })
             .catch(err => {
                 this.dispatchEvent(
                     new ShowToastEvent({
                         title: 'Error',
-                        message: err.body ? err.body.message : err,
+                        message:
+                            err && err.body ? err.body.message : err.message,
                         variant: 'error'
                     })
                 );
+                this.dispatchEvent(new CloseActionScreenEvent());
             });
     }
 }
